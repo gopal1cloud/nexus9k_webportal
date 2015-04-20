c8e11b1f30b7d13828aa3f984e6f704bef005ae7
Arun Mittal <mittal.talk@gmail.com>
2015-04-20 11:40:32 +0530

    moved qa file to qa floder

--- nexus9k/urls.py
+++ nexus9k/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     url(r'^profile', TemplateView.as_view(template_name='profile.html'), name='profile'),
     url(r'^articles', TemplateView.as_view(template_name='articles.html'), name='articles'),
     url(r'^article', TemplateView.as_view(template_name='article.html'), name='article'),
-    url(r'^questions', TemplateView.as_view(template_name='questions.html'), name='questions'),
-    url(r'^question', TemplateView.as_view(template_name='question.html'), name='question'),
-    url(r'^new_question', TemplateView.as_view(template_name='newquestion.html'), name='new_question'),
+    url(r'^questions', TemplateView.as_view(template_name='qa/questions.html'), name='questions'),
+    url(r'^question', TemplateView.as_view(template_name='qa/question.html'), name='question'),
+    url(r'^new_question', TemplateView.as_view(template_name='qa/newquestion.html'), name='new_question'),
     url(r'^login', TemplateView.as_view(template_name='registration/login.html'), name='login'),
     url(r'^register', TemplateView.as_view(template_name='registration/registration_form.html'), name='register'),
 ]
